fbd64304810bb986fbab59aa712d24d0f95ac5f3
moeeahmed <m.ahamedbusiness@gmail.com>
2024-12-11 18:57:09 +0000

    added packages and whitelist images domains

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["scontent-man2-1.cdninstagram.com", "cdninstagram.com"], // Add the domain here
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
